c7ce8982231789931b5b76d0ef21a38acc151cc8
Danae Dekker <info@danaedekker.com>
2023-10-25 17:03:59 +0200

    Updated template logic

--- src/js/templates.js
+++ src/js/templates.js
@@ -2,46 +2,47 @@ const fs = require('fs');
 const mustache = require('mustache');
 
 
+// Render a template
+function _render($el, template, view) {
+  console.log(view);
+  $el.html(mustache.render(template, view));
+}
+
+
 // Reisplanner template
 function reisplanner($el, view) {
   let template = fs.readFileSync('src/templates/reisplanner.mustache', 'utf-8');
-  let render = mustache.render(template, view);
-  $el.html(render);
+  _render($el, template, view);
 }
 
 // Meldingen template
 function meldingen($el, view) {
   let template = fs.readFileSync('src/templates/meldingen.mustache', 'utf-8');
-  let render = mustache.render(template, view);
-  $el.html(render);
+  _render($el, template, view);
 }
 
 // Tickets template
 function tickets($el, view) {
   let template = fs.readFileSync('src/templates/tickets.mustache', 'utf-8');
-  let render = mustache.render(template, view);
-  $el.html(render);
+  _render($el, template, view);
 }
 
 // Dienstregeling template
 function dienstregeling($el, view) {
   let template = fs.readFileSync('src/templates/dienstregeling.mustache', 'utf-8');
-  let render = mustache.render(template, view);
-  $el.html(render);
+  _render($el, template, view);
 }
 
 // Stations template
 function stations($el, view) {
   let template = fs.readFileSync('src/templates/stations.mustache', 'utf-8');
-  let render = mustache.render(template, view);
-  $el.html(render);
+  _render($el, template, view);
 }
 
 // Not found template
 function notFound($el, view) {
   let template = fs.readFileSync('src/templates/404.mustache', 'utf-8');
-  let render = mustache.render(template, view);
-  $el.html(render);
+  _render($el, template, view);
 }
 
 
