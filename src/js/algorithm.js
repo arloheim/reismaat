4941046c41027548689c5780e6e3d1795fa487e7
Danae Dekker <info@danaedekker.com>
2023-11-10 16:21:41 +0100

    Updated and improved the RAPTOR algorithm, still needs some bugfixing

--- src/js/algorithm.js
+++ src/js/algorithm.js
@@ -124,106 +124,75 @@ class RaptorAlgorithm
 
   // Return possible journeys between two nodes
   calculate(departureNode, arrivalNode, dateTime) {
-    // Get all connections from the departure node
-    let connections = this._scan(departureNode);
-
-    // Check if there are connections to the arrival node
-    if ([...connections.keys()].find(n => n.id === arrivalNode.id) === undefined) {
-      console.warn(`Could not find any journeys between ${departureNode.name} and ${arrivalNode.name}`);
-      return [];
-    }
-
-    // Iterate over the connections from the departure node to the arrival node
+    // Get all labels from the departure node
+    let kLabels = this._scan(departureNode);
+    
+    // Iterate over the labels from the departure node to the arrival node
     let journeys = [];
-    for (let k of connections.get(arrivalNode).keys()) {
-      let legs = [];
-      let toNode = arrivalNode;
+    for (let k = 1; k < kLabels.length; k ++) {
+      // Break if there are no labels in this round
+      if (kLabels[k].size === 0)
+        break;
 
-      // Iterate over the connections
-      while (k >= 0)  {
-        // Get the connection at the k-th trip
-        let connection = connections.get(toNode)?.get(k);
-        if (k === 0 && connection === undefined)
-          break;
-
-        // Prepend a new leg
-        if (connection instanceof feed.Route) {
-          legs.unshift(new RouteLeg(connection));
-          toNode = connection.stops[0].node;
-          k --;
-        } else if (connection instanceof feed.Transfer) {
-          legs.unshift(new TransferLeg(connection._alignToOppositeNode(toNode)));
-          toNode = connection.getOppositeNode(toNode);
-        } else {
-          console.warn(`Found unrecognized connection type`, connection);
-          break;
-        }
-      }
-
-      // Append the legs to the journey
-      journeys.push(new Journey(journeys.length, legs, dateTime ?? dayjs()));
+      // Get the trace of k trips and append it to the journeys
+      let trace = this._traceBack(kLabels, arrivalNode, k).slice(1);
+      if (trace.length > 0)
+        journeys.push(new Journey(journeys.length, trace.map(t => t.leg), dateTime ?? dayjs()));
     }
 
     // Sort the journeys by total time
     journeys.sort((a, b) => a.duration - b.duration);
 
     // Return the journeys
+    if (journeys.length === 0)
+      console.warn(`Could not find any journeys between ${departureNode.name} and ${arrivalNode.name}`);
     return journeys;
   }
 
-  // Return all possible connections at the specified departure node
-  _scan(departureNode) {
-    // kTimes[i][node] denotes the time it takes to travel to node with up to i trips
-    let kTimes = new Map([[0, new Map(this.feed.nodes.map(n => [n, Infinity]))]]);
-    kTimes.get(0).set(departureNode, 0);
+  // Return if a node is before another node in a route
+  _isNodeBefore(route, node1, node2) {
+    return route.getStopIndexAtNode(node1) < route.getStopIndexAtNode(node2);
+  }
 
-    // bestTimes[node] denotes the shortest time it takes to travel to node regardless of which trip
-    let bestTimes = new Map(this.feed.nodes.map(n => [n, Infinity]));
-    bestTimes.set(departureNode, 0);
+  // Return all possible labels at the specified departure node
+  _scan(departureNode, rounds = 10) {
+    // kLabels[i][node] denodes the label for node up to i trips
+    let kLabels = Array.from({length: rounds}, () => new Map());
+    kLabels[0].set(departureNode, {cumulativeTime: 0});
 
-    // kConnections[node][i] denotes the connection from departureNode to node at the i-th trip
-    let kConnections = new Map();
+    // bestLabels[node] denotes the shortest time it takes to travel to node regardless of which trip
+    let bestLabels = new Map();
+    bestLabels.set(departureNode, {cumulativeTime: 0});
 
     // markedNodes denotes a set of nodes for which the time is improved at the previous round
-    let markedNodes = [departureNode];
+    let markedNodes = new Set([departureNode]);
 
     // Iterate over the transfers from the departure node
     for (let transfer of departureNode.transfers) {
-      // Apply the time to the transfer
       let transferNode = transfer.getOppositeNode(departureNode);
-      transfer = transfer._copy();
 
       // Update the time to travel to the transfer node
-      kTimes.get(0).set(transferNode, transfer.cumulativeTime);
-      bestTimes.set(transferNode, transfer.cumulativeTime);
-
-      // Update the connection of the transfer node
-      if (!kConnections.has(transferNode))
-        kConnections.set(transferNode, new Map());
-      kConnections.get(transferNode).set(0, transfer);
+      let alignedTransfer = transfer._alignToNode(departureNode);
+      kLabels[0].set(transferNode, {node: departureNode, transfer: alignedTransfer, leg: new TransferLeg(alignedTransfer), cumulativeTime: alignedTransfer.cumulativeTime});
+      bestLabels.set(transferNode, {node: departureNode, transfer: alignedTransfer, leg: new TransferLeg(alignedTransfer), cumulativeTime: alignedTransfer.cumulativeTime});
 
       // Mark the transfer node
-      if (markedNodes.find(n => n.id === transferNode.id) === undefined)
-        markedNodes.push(transferNode);
+      markedNodes.add(transferNode);
     }
 
     // Iterate over the rounds while there are nodes marked
-    let k = 0;
-    while (markedNodes.length > 0) {
-      k ++;
-      kTimes.set(k, new Map(this.feed.nodes.map(n => [n, Infinity])));
-
-      if (k > 100)
-        break;
-
+    let queue = new Map();
+    for (let k = 1; k < rounds; k ++) {
       // First stage: accumulate routes serving marked nodes from previous rounds
-      let queue = new Map();
-      while (markedNodes.length > 0) {
-        let node = markedNodes.shift();
-        for (let {route} of node.routesExcludingNonHalts) {
+      queue.clear();
+      for (let node of markedNodes) {
+        for (let {route, stop} of node.routesExcludingNonHalts) {
+          if (stop.last)
+            continue;
           if (!queue.has(route.id) || this._isNodeBefore(route, node, queue.get(route.id)))
             queue.set(route.id, node);
         }
+        markedNodes.delete(node);
       }
 
       // Second stage: examine the routes
@@ -231,25 +200,46 @@ class RaptorAlgorithm
         let route = this.feed.getRoute(routeId);
 
         // Apply the time to the route
-        route = route._sliceBeginningAtNode(routeNode)._withInitialTime(kTimes.get(k - 1).get(routeNode) + (k > 1 ? 60 : 0));
+        route = route._sliceBeginningAtNode(routeNode)._withInitialTime(kLabels[k - 1].get(routeNode).cumulativeTime + (k > 1 ? 60 : 0));
 
         // Iterate over the stops in the route starting at routeNode
-        for (let stop of route.stops) {
-          // Improve the time of the node of the stop if it is shorter than the current best time
-          if (stop.cumulativeTime < bestTimes.get(stop.node)) {
-            // Update the time to travel to the node of the stop
-            kTimes.get(k).set(stop.node, stop.cumulativeTime);
-            bestTimes.set(stop.node, stop.cumulativeTime);
+        let stopIndex = 1;
+        while (stopIndex < route.stops.length) {
+          let stop = route.stops[stopIndex];
 
-            // Update the connection of the node of the stop
-            if (!kConnections.has(stop.node))
-              kConnections.set(stop.node, new Map());
-            kConnections.get(stop.node).set(k, route._sliceEndingAtNode(stop.node));
+          // Check if there is a faster route to the node of the stop
+          if (kLabels[k - 1].has(stop.node)) {
+            let label = kLabels[k - 1].get(stop.node);
 
-            // Mark the node of the stop
-            if (markedNodes.find(n => n.id === stop.node.id) === undefined)
-              markedNodes.push(stop.node);
+            // Check if the node can be reached faster using the label
+            if (label.cumulativeTime < stop.cumulativeTime) {
+              // Slice the route with the better time
+              route = route._sliceBeginningAtNode(stop.node)._withInitialTime(label.cumulativeTime + (k > 1 ? 60 : 0));
+
+              // Update the route-dependent variables
+              routeNode = stop.node;
+              stopIndex = 0;
+              stop = route.stops[stopIndex];
+            }
           }
+
+          // Improve the time of the node of the stop if it is shorter than the current best time
+          if (stop.cumulativeTime < (bestLabels.get(stop.node)?.cumulativeTime ?? Infinity)) {
+            let routeSoFar = route._sliceEndingAtNode(stop.node);
+
+            // Check if the route has more than one stop
+            if (routeSoFar.stops.length > 1) {
+              // Update the time to travel to the node of the stop
+              kLabels[k].set(stop.node, {node: routeNode, route: routeSoFar, leg: new RouteLeg(routeSoFar), cumulativeTime: stop.cumulativeTime});
+              bestLabels.set(stop.node, {node: routeNode, route: routeSoFar, leg: new RouteLeg(routeSoFar), cumulativeTime: stop.cumulativeTime});
+
+              // Mark the node of the stop
+              markedNodes.add(stop.node);
+            }
+          }
+
+          // Increase the stopIndex
+          stopIndex ++;
         }
       }
 
@@ -257,36 +247,59 @@ class RaptorAlgorithm
       for (let node of markedNodes) {
         // Iterate over the transfers from the node
         for (let transfer of node.transfers) {
-          // Apply the time to the transfer
           let transferNode = transfer.getOppositeNode(node);
-          transfer = transfer._withInitialTime(kTimes.get(k).get(node));
+
+          // Check for cyclic labels
+          let trace = this._traceBack(kLabels, node, k);
+          if (trace.find(label => label.leg instanceof TransferLeg && label.node === transferNode))
+            continue;
+
+          // Apply the time to the transfer
+          transfer = transfer._withInitialTime(kLabels[k].get(node).cumulativeTime);
 
           // Improve the time of the transfer node if it is shorter than the best time
-          if (transfer.cumulativeTime < kTimes.get(k).get(transferNode)) {
+          if (transfer.cumulativeTime < (bestLabels.get(transferNode)?.cumulativeTime ?? Infinity)) {
             // Update the time to travel to the transfer node
-            kTimes.get(k).set(transferNode, transfer.cumulativeTime);
-            bestTimes.set(transferNode, transfer.cumulativeTime);
-
-            // Update the connection of the transfer node
-            if (!kConnections.has(transferNode))
-              kConnections.set(transferNode, new Map());
-            kConnections.get(transferNode).set(k, transfer);
+            let alignedTransfer = transfer._alignToNode(transferNode);
+            kLabels[k].set(transferNode, {node: node, transfer: alignedTransfer, leg: new TransferLeg(alignedTransfer), cumulativeTime: alignedTransfer.cumulativeTime});
 
             // Mark the transfer node
-            if (markedNodes.find(n => n.id === transferNode.id) === undefined)
-              markedNodes.push(transferNode);
+            markedNodes.add(transferNode);
           }
         }
       }
+
+      // If there are no more marked nodes, then stop the algorithm
+      if (markedNodes.size === 0)
+        break;
     }
 
-    // Return the connections
-    return kConnections;
+    // Return the labels
+    return kLabels;
   }
 
-  // Return if a node is before another node in a route
-  _isNodeBefore(route, node1, node2) {
-    return route.getStopIndexAtNode(node1) < route.getStopIndexAtNode(node2);
+  // Trace back a label
+  _traceBack(kLabels, arrivalNode, round) {
+    let trace = [];
+    let previousNode = arrivalNode;
+
+    // Iterate over the labels
+    let k = round;
+    while (k >= 0) {
+      // Get the label at the k-th trip
+      let previousLabel = kLabels[k].get(previousNode);
+      if (previousLabel === undefined)
+        break;
+
+      // Prepend the label
+      trace.unshift(previousLabel);
+      previousNode = previousLabel.node;
+      if (!(previousLabel.leg instanceof TransferLeg))
+        k --;
+    }
+
+    // Return the trace
+    return trace;
   }
 }
 
