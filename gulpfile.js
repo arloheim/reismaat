23616ec1a4276b16969b4cbb99fcbe5770622cae
Danae Dekker <hello@danaedekker.com>
2024-12-22 14:59:20 +0100

    Changed Sass package

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const browserify = require('browserify');
 const buffer = require('vinyl-buffer');
 const gulp = require('gulp');
 const rename = require('gulp-rename');
-const sass = require('gulp-sass')(require('node-sass'));
+const sass = require('gulp-sass')(require('sass'));
 const source = require('vinyl-source-stream');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
@@ -26,7 +26,7 @@ async function js() {
     .pipe(buffer())
     .pipe(gulp.dest('./dist/js'))
     .pipe(sourcemaps.init({loadMaps: true}))
-    //.pipe(uglify())
+    .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/js'));
 }
@@ -35,8 +35,8 @@ async function css() {
   return gulp.src('./src/scss/main.scss')
     .pipe(rename('bundle.css'))
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'condensed', includePaths: ['./node_modules']})
-       .on('error', handleError))
+    .pipe(sass({includePaths: ['./node_modules']})
+      .on('error', handleError))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/css'));
 }
